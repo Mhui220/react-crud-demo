0738abb2a1d50864080955a459628f7264d914df
meihui-220 <84300869+meihui-220@users.noreply.github.com>
2026-04-03 22:47:49 +0800

    update login API url

--- src/api/loginApi.ts
+++ src/api/loginApi.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 export const login = async (user: { username: string; password: string }) => {
-  console.log(import.meta.env.VITE_API_URL)
-  const res = await axios.post("/api/Auth/login", user);
+  const res = await axios.post(`${API_BASE_URL}/Auth/login`, user);
   return res.data;
 };
